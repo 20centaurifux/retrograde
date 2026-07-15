2755abdfe74e9f56f9db9c256c16e0ccc1ddb929
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2026-07-15 06:53:36 +0200

    docs: clarify time conversion docstrings

--- src/retrograde/time.clj
+++ src/retrograde/time.clj
@@ -3,7 +3,7 @@
 (defn ->epoch
   "Converts a java.time.Instant or java.util.Date to epoch seconds.
 
-  Returns nil when instant is nil."
+  Returns nil when `instant` is nil."
   [instant]
   (when instant
     (if (instance? java.time.Instant instant)
@@ -11,7 +11,7 @@
         (quot (.getTime instant) 1000))))
 
 (defn ->instant
-  "Converts epoch seconds to a java.time.Instant.
+  "Converts `epoch` seconds to a java.time.Instant.
 
   Returns nil when epoch is nil."
   [epoch]
